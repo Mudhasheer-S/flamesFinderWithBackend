e41953f03e3f0a1028165099dd2f592d286899c8
Mudhasheer S <mudhasheer0786@gmail.com>
2023-12-24 21:14:10 +0530

    add port to url

--- src/components/Form.js
+++ src/components/Form.js
@@ -46,7 +46,7 @@ class Flames extends React.Component {
 
     const addToDatabase =  async () => {
       
-      await axios.post("https://node-flamesfinder-backend.onrender.com",{personOne:personOne , personTwo:personTwo})
+      await axios.post("https://node-flamesfinder-backend.onrender.com:8087",{personOne:personOne , personTwo:personTwo})
                   .then(response => console.log(response.data))
                   .catch(err => console.error(err))
     }
